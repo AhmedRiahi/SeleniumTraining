9402611696bae6c4d09abad0da68197ed9fc2b60
Ahmed RIAHI <ahmed.riahi@talan.com>
2019-02-21 09:35:24 +0100

    screenshot on test failure

--- src/test/java/cucumber/LoginTest.java
+++ src/test/java/cucumber/LoginTest.java
@@ -1,6 +1,6 @@
 package cucumber;
 
-import cucumber.api.PendingException;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -9,18 +9,27 @@ import cucumber.api.java.en.When;
 import helper.PropertiesManager;
 import helper.locator.PageLocator;
 import helper.locator.PageLocatorsParser;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
+
+@Slf4j
 public class LoginTest {
 
     private WebDriver driver;
     private String username;
     private String password;
     private PageLocator loginPageLocator;
+
+
 
     @Before
     public void before(){
@@ -32,17 +41,27 @@ public class LoginTest {
     }
 
     @After
-    public void after(){
+    public void after(Scenario scenario){
+        if(scenario.isFailed()){
+            try {
+                File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                File targetFile = new File(  scenario.getName() + ".png");
+                Files.copy(scrFile.toPath(), targetFile.toPath());
+            } catch (IOException e) {
+                log.error(e.getMessage(),e);
+            }
+        }
        this.driver.quit();
     }
 
-
+    @Test
     @Given("^username \"([^\"]*)\" and password \"([^\"]*)\"$")
     public void giver_username_and_password(String username,String password){
         this.username = username;
         this.password = password;
     }
 
+    @Test
     @When("^user navigate to \"([^\"]*)\"$")
     public void user_navigate_to(String url) throws Throwable {
         this.driver.get(url);
@@ -50,7 +69,7 @@ public class LoginTest {
 
     @When("^user types username and password$")
     public void user_types_username_and_password() throws Throwable {
-        WebElement usernameInput = this.driver.findElement(loginPageLocator.dom("username").primaryBy());
+        WebElement usernameInput = this.driver.findElement(loginPageLocator.dom("usrname").primaryBy());
         WebElement passwordInput = this.driver.findElement(loginPageLocator.dom("password").primaryBy());
 
         usernameInput.sendKeys(this.username);
