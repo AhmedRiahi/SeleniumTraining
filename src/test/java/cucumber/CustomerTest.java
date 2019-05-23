feca3a5d71ce82b625f27b74af17c0576cb357a4
Ahmed Riahi <contact@ahmedriahi.net>
2019-05-24 00:51:34 +0100

    Update CustomerTest.java

--- src/test/java/cucumber/CustomerTest.java
+++ src/test/java/cucumber/CustomerTest.java
@@ -27,7 +27,7 @@ public class CustomerTest {
 
     @Before
     public void before(){
-        System.setProperty("webdriver.chrome.driver","C:\\soft\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","/root/selenium/chromedriver");
         this.driver = new ChromeDriver();
         PropertiesManager.getInstance().loadProperties();
         PageLocatorsParser.getInstance().parsePageLocators();
